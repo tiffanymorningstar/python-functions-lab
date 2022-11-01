35d27567dfcbab2b1f135361b998424734c79da8
trmorningstar <trmorningstar@gmail.com>
2022-11-01 17:17:31 -0400

    solve challenge 4

--- python_functions.py
+++ python_functions.py
@@ -1,4 +1,32 @@
-def sum_to(n):
+
+# Challenge 1
+
+# def sum_to(n):
   
-  print(n * (n +1) //2)
-sum_to(6)
+#   print(n * (n +1) //2)
+# sum_to(6)
+
+# Challenge 2
+
+# def largest(nums):
+#   nums.sort()
+#   return nums[-1]
+
+# print(largest([5, 34, 18, 4, 7]))
+
+# Challenge 3
+
+# def occurrences(string1, string2):
+#   return string1.count(string2)
+
+# print(occurrences('fleep floop', 'e'))
+
+# Challenge 4
+
+# def product(*args):
+#   product = 1
+#   for arg in args:
+#     product *= arg
+#   return product
+
+# print(product(-1, 4)) 
